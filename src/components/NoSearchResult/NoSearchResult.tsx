9f9894262b577bfde5d9f8d0090d4f30506fba6b
gkubiak92 <gkubiak92@gmail.com>
2020-10-05 12:24:33 +0200

    Added styles to paper

--- src/components/NoSearchResult/NoSearchResult.tsx
+++ src/components/NoSearchResult/NoSearchResult.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Grid, Paper, Typography } from '@material-ui/core';
 import NoSearchResultIcon from 'assets/svg/no-search-result.svg';
+import { useStyles } from './styles';
 
-const NoSearchResult = () => (
-  <Grid container justify='center' alignItems='center'>
-    <Grid item xs={12} lg={6} component={Paper} elevation={0}>
-      <Grid container direction='column' justify='center' alignItems='center'>
-        <img src={NoSearchResultIcon} alt='no search result' />
-        <Typography gutterBottom variant='h5' component='h2'>
-          Ooops... It's empty here
-        </Typography>
-        <Typography variant='body2' color='textSecondary' component='p'>
-          There are no products on the list
-        </Typography>
+const NoSearchResult = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container justify='center' alignItems='center'>
+      <Grid item className={classes.paper} xs={12} lg={6} component={Paper} elevation={0}>
+        <Grid
+          container
+          className={classes.paperContainer}
+          direction='column'
+          justify='center'
+          alignItems='center'
+        >
+          <img src={NoSearchResultIcon} alt='no search result' />
+          <Typography gutterBottom variant='h5' component='h2'>
+            Ooops... It's empty here
+          </Typography>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            There are no products on the list
+          </Typography>
+        </Grid>
       </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
 
 export default NoSearchResult;
---
+++ src/components/NoSearchResult/styles.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme) => ({
+  paper: {
+    borderRadius: 8,
+    height: 344,
+  },
+  paperContainer: {
+    height: '100%',
+  },
+}));
