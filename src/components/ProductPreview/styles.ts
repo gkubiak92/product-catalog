ba683d75475b78a287eab0cd9d6c33283e51bdbc
gkubiak92 <gkubiak92@gmail.com>
2020-10-06 16:10:00 +0200

    Added index to product preview dialog makeStyles to fix order of css injection

--- src/components/ProductPreview/styles.ts
+++ src/components/ProductPreview/styles.ts
@@ -1,33 +1,36 @@
 import { makeStyles } from '@material-ui/core';
 
-export const useStyles = makeStyles((theme) => ({
-  dialogContainer: {},
-  dialogBox: {
-    background: 'transparent',
-  },
-  titleContainer: {
-    position: 'relative',
-    padding: 0,
-  },
-  closeButton: {
-    position: 'absolute',
-    top: 16,
-    right: 16,
-    padding: 0,
-  },
-  image: {
-    width: '100%',
-  },
-  dialogContent: {
-    padding: 24,
-    [theme.breakpoints.up('md')]: {
-      padding: '24px 32px 56px 32px',
+export const useStyles = makeStyles(
+  (theme) => ({
+    dialogContainer: {},
+    dialogBox: {
+      background: 'transparent',
     },
-  },
-  productTitle: {
-    fontSize: 24,
-  },
-  productDesc: {
-    fontSize: 18,
-  },
-}));
+    titleContainer: {
+      position: 'relative',
+      padding: 0,
+    },
+    closeButton: {
+      position: 'absolute',
+      top: 16,
+      right: 16,
+      padding: 0,
+    },
+    image: {
+      width: '100%',
+    },
+    dialogContent: {
+      padding: 24,
+      [theme.breakpoints.up('md')]: {
+        padding: '24px 32px 56px 32px',
+      },
+    },
+    productTitle: {
+      fontSize: 24,
+    },
+    productDesc: {
+      fontSize: 18,
+    },
+  }),
+  { index: 1 }
+);
