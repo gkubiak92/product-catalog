71184a49b5864dd4b01eafc4abffa777ac449211
gkubiak92 <gkubiak92@gmail.com>
2020-10-07 19:39:36 +0200

    Fixed products test

--- src/app/products/Products.test.tsx
+++ src/app/products/Products.test.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { renderWithRedux } from 'tests';
 
 import { Products } from './Products';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('Products', () => {
   test('Displays layout header', async () => {
-    const { container } = renderWithRedux(<Products />);
+    const { container } = renderWithRedux(
+      <Router>
+        <Products />
+      </Router>
+    );
 
     expect(container.querySelector('.MuiAppBar-root')).toBeInTheDocument();
   });
